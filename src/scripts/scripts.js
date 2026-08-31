3933a36dd276382b83d18597d212594d30fe6703
Gabriel Cafe <gabrielcafens@gmail.com>
2026-08-30 21:18:10 -0300

    fix: menu mobile nao fechava ao clicar num link de navegacao
    Clicar num item do #mobile_nav_list navegava para a secao mas deixava
    o menu hamburguer aberto e o icone travado em X, exigindo um segundo
    toque manual pra fechar.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
     $('#mobile_btn').on('click', function (){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
-        
+
+    });
+
+    $('#mobile_nav_list a').on('click', function () {
+        $('#mobile_menu').removeClass('active');
+        $('#mobile_btn').find('i').removeClass('fa-x');
     });
 });
 
